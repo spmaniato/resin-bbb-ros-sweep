220a69a7ed9c60bd57f8ead0137565a8af371406
Spyros Maniatopoulos <spmaniato@gmail.com>
2017-05-13 16:31:55 -0700

    [entrypoint] Set ROS_HOSTNAME to device hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # Set up ROS environment
 source "${ROS_INSTALL_DIR}/setup.bash"
 
-export ROS_HOSTNAME=localhost
+# Set up ROS networking-related parameters
+export ROS_HOSTNAME="${HOSTNAME}"
+echo -e "127.0.1.1\t${HOSTNAME}" >> /etc/hosts
 
 exec "$@"
